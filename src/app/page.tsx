cbb7cc566e2abdb101d84cc593866bc6b64e1596
JairoGC16 <jairoirineo16@outlook.com>
2025-08-17 18:44:24 -0600

    Add select-none class to main for improved text selection handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="font-sans bg-slate-50 min-h-screen flex flex-col items-center justify-center px-2 sm:px-4 md:px-8 lg:px-24 xl:px-40 pt-16">
+      <main className="font-sans bg-slate-50 min-h-screen flex flex-col items-center select-none justify-center px-2 sm:px-4 md:px-8 lg:px-24 xl:px-40 pt-16">
         <div className="w-full max-w-6xl flex flex-col gap-8 md:gap-12 text-slate-900">
           <Hero />
           <About />
