4fd978d4167357122aa00305a5f8a777c83bef55
JairoGC16 <jairoirineo16@outlook.com>
2025-08-17 18:44:31 -0600

    Refactor Footer component: update social link styles for consistent hover effects

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,31 +7,31 @@ const socialLinks = [
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/jairo-gonzalez-160402mx/",
     icon: <Linkedin className="w-6 h-6" />,
-    className: "hover:text-[#0e76a8]",
+    className: "text-[#0e76a8]",
   },
   {
     name: "GitHub",
     url: "https://github.com/JairoGC16",
     icon: <Github className="w-6 h-6" />,
-    className: "hover:text-[#ffffff]"
+    className: "text-[#ffffff]"
   },
   {
     name: "WhatsApp",
     url: "https://wa.me/524435290635?text=Hola!%20Estoy%20interesado%20en%20tu%20portafolio",
     icon: <FaWhatsapp className="w-6 h-6" />,
-    className: "hover:text-[#25D366]",
+    className: "text-[#25D366]",
   },
   {
     name: "Correo",
     url: "mailto:jairoirineo1604@gmail.com",
     icon: <Mail className="w-6 h-6" />,
-    className: "hover:text-[#EA4335]",
+    className: "text-[#EA4335]",
   },
   {
     name: "CV",
     url: "https://drive.google.com/uc?export=download&id=12BQRYmrREocCnh2IqCWlBtdD2E6vIW_H",
     icon: <FaFileDownload className="w-6 h-6" />,
-    className: "hover:text-blue-500"
+    className: "text-blue-500"
   }
 ];
 
@@ -47,7 +47,7 @@ const Footer = () => {
               href={link.url}
               target="_blank"
               rel="noopener noreferrer"
-              className={`text-slate-400 transition-colors duration-300 ${link.className}`}
+              className={`text-slate-400 transition-colors duration-300 hover:${link.className} focus:${link.className} focus:outline-none`}
             >
               {link.icon}
             </a>
