70a31f8372c41d6cb0fc9a0b36733d938af5ec41
JairoGC16 <jairoirineo1604@gmail.com>
2025-08-19 16:19:25 -0600

    build static site

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ['res.cloudinary.com'],
   },
+  images: {
+    unoptimized: true, // necesario para exportar imágenes
+  },
+  basePath: "/portfile", // nombre de tu repo en GitHub
 };
 
 module.exports = nextConfig;
